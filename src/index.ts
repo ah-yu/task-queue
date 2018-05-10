c37b8a0bace4c277d7304b3a20ff47a21a26e7f0
buji <zheyuzhang03@gmail.com>
2018-05-10 10:09:45 +0800

    fixes: promise bug

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,9 @@ export default class TaksQueue {
             );
 
             const promisefy = function(func: Function) {
-                return Promise.resolve(func);
+                return Promise.resolve().then(() => {
+                    func();
+                });
             };
 
             const promises = [];
